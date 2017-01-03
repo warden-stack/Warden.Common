ee692f427a74d455963f684615fe6e6764b36b03
Piotr Gankiewicz <spetz90@gmail.com>
2017-01-03 19:32:01 +0100

    Improved domain

--- Domain/Entity.cs
+++ Domain/Entity.cs
@@ -1,23 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using Warden.Common.Events;
-
-namespace Warden.Common.Domain
+﻿namespace Warden.Common.Domain
 {
     public abstract class Entity : IEntity
     {
-        private readonly Dictionary<Type, IEvent> _events = new Dictionary<Type, IEvent>();
-
-        public IEnumerable<IEvent> Events => _events.Values;
-
-        protected void AddEvent(IEvent @event)
-        {
-            _events[@event.GetType()] = @event;
-        }
-
-        public void ClearEvents()
-        {
-            _events.Clear();
-        }
     }
 }
